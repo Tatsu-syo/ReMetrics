deb6bcf1972d14e2e1084ea0132ee79e77cdf2be
Tatsuhiko Syoji <rxk16664@nifty.ne.jp>
2023-08-13 00:49:11 +0900

    年号とバージョンの変更(プログラム)

--- program/resource.h
+++ program/resource.h
@@ -1,5 +1,5 @@
 /*
-Re-Metrics (C) 2012-2017,2020,2021,2022 Tatsuhiko Shoji
+Re-Metrics (C) 2012-2017,2020-2023 Tatsuhiko Shoji
 The sources for Re-Metrics are distributed under the MIT open source license
 */
 //{{NO_DEPENDENCIES}}
